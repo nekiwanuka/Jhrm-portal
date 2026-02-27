07c712858d58e5b0466decdc3a618b7895992f3d
nekiwanuka <nekiwanuka@gmail.com>
2026-02-27 15:16:27 +0300

    Polish login UI with branded header

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,5 +3,23 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Email', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    username = forms.CharField(
+        label='Email',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Email address',
+                'autocomplete': 'username',
+                'autofocus': 'autofocus',
+            }
+        ),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password',
+                'autocomplete': 'current-password',
+            }
+        )
+    )
